5500ee9136f6ffa22eb9e45d5aa6beac6f056c8b
김연구 <yeongoo.kim@qualson.com>
2022-02-09 08:35:15 +0900

    initial setting

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// content -> tailwindcss를 사용하는 곳 작성 pages 전체 components 전체
